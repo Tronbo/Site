e3897fdfa5ec6ee851fd4cf375a4c877c32268c8
Eddy Zhu <67758739+Tronbo@users.noreply.github.com>
2021-08-20 10:09:26 -0400

    Update typewriter.js

--- JS/typewriter.js
+++ JS/typewriter.js
@@ -1,9 +1,9 @@
 var span, textArr, textIndex, textToShow, textToShowLen, letterIndex, textTimer, letterTimer, textDelay, letterDelay, emptyTimer;
 
 textArr = [
-  'Eddy',
+  'Eddy.',
   'Creative.',
-  'Fredward',
+  'Fredward.',
   'a Developer.',
   'Curious.',
   'a Swimmer.',
